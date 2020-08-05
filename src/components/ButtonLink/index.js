9c0b6c48b6ff288f249aeeabf84ba5de1d658f0f
Cris <pg2006pe@hotmail.com>
2020-08-05 05:07:22 -0300

    Second Class

--- src/components/ButtonLink/index.js
+++ src/components/ButtonLink/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 function ButtonLink(props) {
     return (
-        <a href={props.href} className={props.className}>
+        <Link to={props.to} className={props.className}>
             {props.children}
-        </a>
+        </Link>
     );
 }
+
+ButtonLink.defaultProps = {
+    to: '/',
+    className: '',
+};
+
+ButtonLink.propTypes = {
+    className: PropTypes.string,
+    to: PropTypes.string,
+    children: PropTypes.node.isRequired,
+};
 
 export default ButtonLink;
